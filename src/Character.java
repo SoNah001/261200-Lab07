2c6e20208a7eda9fe0af7b3a0e70c37476aa44f7
Supanat <k.suphanuch23@gmail.com>
2026-01-30 15:31:17 +0700

    #

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
 public class Character {
+    private int health;
+    private int stamina;
+    private final int maxHealth;
+    private final int maxStamina;
+
+    //Constructor
+    public Character(int health, int stamina) {
+        this.health = health;
+        this.stamina = stamina;
+        this.maxHealth = health;
+        this.maxStamina = stamina;
+        System.out.println("Character created with " + this.health + " health and " + this.health + " stamina.");
+    }
+
+    //Getter
+    public void getStatus() {
+        System.out.println("Character's health: " + this.health + ", stamina: " + this.stamina + ".");
+    }
+
+    //Method
+    public void swingSword(){
+        if(this.stamina <= 0){
+            System.out.println("Your character needs to cool down.");
+            this.stamina = 0;
+        }
+        else{
+            this.stamina = this.stamina - 10;
+            System.out.println("Character swings the sword. Stamina is now " + this.stamina + ".");
+            if(this.stamina <= 0){
+                System.out.println("Your character needs to cool down.");
+                this.stamina = 0;
+            }
+        }
+    }
+    public void takeDamage(int damageAmount){
+        if(health <= 0){
+            System.out.println("Character is dead.");
+            this.health = 0;
+        }
+        else{
+            this.health = this.health - damageAmount;
+            System.out.println("Character takes " + damageAmount + " damage. " + "Health is now " + this.health + ".");
+            if(health <= 0){
+                System.out.println("Character is dead.");
+                this.health = 0;
+            }
+        }
+    }
+    public void rest(){
+        this.health = this.maxHealth;
+        this.stamina = this.maxStamina;
+        System.out.println("Character has rested. Health and stamina restored to maximum.");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        Character Bob = new Character(100,50);
+        Bob.swingSword();
+        Bob.swingSword();
+        Bob.swingSword();
+        Bob.swingSword();
+        Bob.swingSword();
+        Bob.takeDamage(30);
+        Bob.takeDamage(50);
+        Bob.takeDamage(20);
+        Bob.rest();
+        Bob.getStatus();
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    }
 }
